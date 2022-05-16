e4b480b60f8a146e4249ecfdff16bea63a203b54
Guilherme Colares <gcolares95@gmail.com>
2022-05-16 00:32:08 -0300

    Finalizando Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri';
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri';
 
 export function Header() {
     return (
@@ -48,6 +48,38 @@ export function Header() {
                 />
 
                 <Icon as={RiSearchLine} fontSize="20" />
+            </Flex>
+
+            {/* Icones e Avatar */}
+            <Flex
+                align="center"
+                ml="auto"
+            >
+                <HStack
+                  spacing="8"
+                  mx="8"
+                  pr="8"
+                  py="1"
+                  color="gray.300"
+                  borderRightWidth={1}
+                  borderColor="gray.700"
+                >
+                    <Icon as={RiNotificationLine} fontSize="20" />
+                    <Icon as={RiUserAddLine} fontSize="20" />
+                </HStack>
+
+                <Flex 
+                    align="center"
+                >
+                    <Box mr="4" textAlign="right">
+                        <Text>Guilherme Araujo</Text>
+                        <Text color="gray.300" fontSize="small">
+                            gcolares95@gmail.com
+                        </Text>
+                    </Box>
+
+                    <Avatar size="md" name="Guilherme Araujo" src="https://github.com/gcolares95.png"/>
+                </Flex>
             </Flex>
         </Flex>
     );
