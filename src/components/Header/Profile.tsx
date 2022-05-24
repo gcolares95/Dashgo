4d8dc0d8a7d2bba54f58572e7119bc9f3630874c
Guilherme Colares <gcolares95@gmail.com>
2022-05-23 23:22:55 -0300

    Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,16 +1,23 @@
 import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
+
     return (
         <Flex
             align="center"
         >
-            <Box mr="4" textAlign="right">
-                <Text>Guilherme Araujo</Text>
-                <Text color="gray.300" fontSize="small">
-                    gcolares95@gmail.com
-                </Text>
-            </Box>
+            {showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Guilherme Araujo</Text>
+                    <Text color="gray.300" fontSize="small">
+                        gcolares95@gmail.com
+                    </Text>
+                </Box>
+            )}
 
             <Avatar size="md" name="Guilherme Araujo" src="https://github.com/gcolares95.png" />
         </Flex>
