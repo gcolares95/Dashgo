02d1d938353079f51c2c0a9bf757bb627782ac47
Guilherme Colares <gcolares95@gmail.com>
2022-05-29 22:53:48 -0300

    Listagem responsiva

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -11,18 +11,19 @@ export default function CreateUser() {
             <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
                 <Sidebar />
 
-                <Box flex="1" borderRadius={8} bgColor="gray.800" p="8">
+                <Box flex="1" borderRadius={8} bgColor="gray.800" p={["6", "8"]}>
                     <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
 
                     <Divider my="6" borderColor="gray.700" />
 
                     {/* Formulário */}
                     <VStack spacing="8">
-                        <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+                        <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
                             <Input name="name" label="Nome completo" />
                             <Input name="email" type="email" label="E-mail" />
+                            <Input name="email" type="email" label="E-mail" />
                         </SimpleGrid>
-                        <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+                        <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
                             <Input name="password" type="password" label="Senha" />
                             <Input name="password_confirmation" type="password" label="Confirmação da senha" />
                         </SimpleGrid>
@@ -33,7 +34,6 @@ export default function CreateUser() {
                             <Button colorScheme="whiteAlpha">Cancelar</Button>
                             <Button colorScheme="pink">Salvar</Button>
                         </HStack>
-
                     </Flex>
                 </Box>
             </Flex>
