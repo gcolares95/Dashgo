5f70d33bffc606e0ade087b48bb1b65e62771ba0
Guilherme Colares <gcolares95@gmail.com>
2022-06-08 01:36:47 -0300

    Factories e seeds

--- src/services/mirage/mirage.ts
+++ src/services/mirage/mirage.ts
@@ -1,5 +1,6 @@
 // Configuração do mirage/servidor do mirage
-import { createServer, Model } from 'miragejs';
+import { createServer, Factory, Model } from 'miragejs';
+import { faker } from '@faker-js/faker';
 
 type User = {
     name: string;
@@ -11,8 +12,25 @@ export function makeServer() {
     const server = createServer({
         models: {                                  // quais dados queremos armazenar no "banco ficticio" que o mirage cria
             user: Model.extend<Partial<User>>({})  // user é como se fosse uma tabela no banco
-    },                                             // <Partial<User> -> podemos usar user sem informar todos os campos necessariamente 
-        
+        },                                         // <Partial<User> -> podemos usar user sem informar todos os campos necessariamente 
+
+        factories: {                               // formas de gerar dados em "massa"
+            user: Factory.extend({                 // precisa ser o mesmo nome do model, nesse caso "user"
+                name(i: number) {
+                    return `User ${i + 1}`
+                },
+                email() {
+                    return faker.internet.email().toLowerCase();
+                },
+                createdAt() {                      // mesmo colocando em camelCase, mirage consegue identificarn que é created_at
+                    return faker.date.recent(10)   // retorna data a partir de 10 dias da data atual
+                }
+            })
+        },
+        seeds(server) {                            // criar dados assim que o servidor do mirage for inicializado
+            server.createList('user', 200)         // 'user' é o nome do factorie criado e 200 a quantidade de usuários
+        },
+
         routes() {                                 // rotas que teremos no mirage
             this.namespace = 'api';                // setar o caminho que a aplicação vai precisar acessar para acessar as rotas do mirage 
             this.timing = 750;                     // Toda chamada a api do mirage demore 750 millisegundos para testar loading, etc
